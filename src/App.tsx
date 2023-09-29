505ce3355f392382fe2d36d5745ce18c1eca6a5e
Fernando Rodrigues <Fernando.hardd@hotmail.com>
2023-09-29 16:54:57 -0300

    problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import { Header } from './style.app'
-import { useState, useEffect } from 'react'
-import { api } from './lib/axios'
+import { useState } from 'react'
+
+const api = {
+  key: '92ad08ad99cfb8a7ae1831156d2a75c2',
+  base: 'https://api.openweathermap.org/data/2.5/',
+}
 
 export const App = () => {
   const [search, setSearch] = useState('') // estado da pesquisa setado sem valor inicial
+  const [weather, setWeather] = useState({})
 
   const searchPressed = () => {
-    console.log(search)
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`) // chamando a func fetch e passando as info da const api (base e key)
+      .then((res) => res.json()) // transforma o res em json
+      .then((result) => {
+        setWeather(result)
+        console.log(setWeather)
+        console.log(weather)
+      })
   }
-
-  useEffect(() => {
-    const getRequisicao = async () => {
-      const response = await api.get(
-        `/?q=London&appid=92ad08ad99cfb8a7ae1831156d2a75c2`,
-      )
-
-      console.log(response)
-    }
-    getRequisicao()
-  }, [])
 
   return (
     <>
@@ -31,8 +31,8 @@ export const App = () => {
             onChange={(e) => setSearch(e.target.value)} // função onChange pegando o valor do input e setando pra variavel setSearch
           ></input>
           <button onClick={searchPressed}>Search</button>
-          <p>São Paulo, Brazil</p>
-          <p>26°C</p>
+          <p></p>
+          <p></p>
         </form>
       </Header>
     </>
--- src/lib/axios.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export const api = axios.create({
-  baseURL: 'https://api.openweathermap.org/data/2.5/weather',
-  // headers: { Authorization: import.meta.env.API_KEY },
-})
